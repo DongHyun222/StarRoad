7145b5a850e77d55cb045ebcc9efd4d0025c28d8
serajang99 <serajang1679@gmail.com>
2023-09-06 10:33:24 +0900

    feat: jsp 구조

--- src/main/java/com/kb04/starroad/Controller/ProductController.java
+++ src/main/java/com/kb04/starroad/Controller/ProductController.java
@@ -1,9 +1,13 @@
 package com.kb04.starroad.Controller;
 
+import com.kb04.starroad.Dto.ProductDto;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
 
 @RestController
 public class ProductController {
@@ -14,7 +18,11 @@ public class ProductController {
     }
 
     @GetMapping("/api/product")
-    public String product_list() {
+    public String product_list(Model model) {
+        ArrayList<ProductDto> productDtos = new ArrayList<>();
+        ProductDto dto = new ProductDto();
+        productDtos.add(dto);
+        model.addAttribute("product_items", productDtos);
         return "product";
     }
 }
--- src/main/webapp/WEB-INF/views/product/product.jsp
+++ src/main/webapp/WEB-INF/views/product/product.jsp
@@ -1,12 +1,22 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
          pageEncoding="UTF-8" %>
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <!DOCTYPE html>
 <html>
 <head>
     <meta charset="UTF-8">
-    <title>BOARD</title>
+    <title>예적금 상품 추천</title>
+    <link rel="stylesheet" type="text/css" href="${path}/resources/static/css/product.css">
 </head>
 <body>
-<h1>board.jsp</h1>
+<h1>예적금 상품 추천</h1>
+<div id="product_search"></div>
+<div id="product_list">
+    <ul>
+        <c:forEach items="${product_items}" var="item">
+            <li>${item}</li>
+        </c:forEach>
+    </ul>
+</div>
 </body>
 </html>
