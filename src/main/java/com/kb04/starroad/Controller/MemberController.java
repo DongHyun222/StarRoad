91c7759ab60312636f8a76e83b8f73b2ec9dac9b
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2023-09-10 19:38:26 +0900

    fix: MemberContoller 수정

--- src/main/java/com/kb04/starroad/Controller/MemberController.java
+++ src/main/java/com/kb04/starroad/Controller/MemberController.java
@@ -1,6 +1,5 @@
 package com.kb04.starroad.Controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
