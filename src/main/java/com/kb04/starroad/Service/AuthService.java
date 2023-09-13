3f3cf9698f72b041a723742bb5b1344fbb643ce4
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2023-09-13 14:26:56 +0900

    feat: 로그인기능 entity->Dto수정

--- src/main/java/com/kb04/starroad/Service/AuthService.java
+++ src/main/java/com/kb04/starroad/Service/AuthService.java
@@ -1,5 +1,6 @@
-package com.kb04.starroad.login;
+package com.kb04.starroad.Service;
 
+import com.kb04.starroad.Dto.auth.LoginRequestDto;
 import com.kb04.starroad.Entity.Member;
 import com.kb04.starroad.Repository.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,7 +12,7 @@ public class AuthService {
     private MemberRepository memberRepository;
 
     //로그인 기능
-    public Member authenticate(String id, String password) {
-        return memberRepository.findByIdAndPassword(id, password).orElse(null);
+    public Member authenticate(LoginRequestDto loginRequestDto) {
+        return memberRepository.findByIdAndPassword(loginRequestDto.getId(), loginRequestDto.getPassword()) .orElse(null);
     }
 }
