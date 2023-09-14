09e3e406da6b7b902056eca5886624608e166ccb
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2023-09-14 17:23:30 +0900

    feat: 댓글생성 기능구현

--- src/main/java/com/kb04/starroad/Service/BoardService2.java
+++ src/main/java/com/kb04/starroad/Service/BoardService2.java
@@ -1,8 +1,11 @@
 package com.kb04.starroad.Service;
 
 import com.kb04.starroad.Dto.board.BoardRequestDto;
+import com.kb04.starroad.Dto.board.BoardResponseDto;
+import com.kb04.starroad.Dto.board.CommentDto;
 import com.kb04.starroad.Entity.Board;
 import com.kb04.starroad.Repository.BoardRepository;
+import com.kb04.starroad.Repository.CommentRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -13,6 +16,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 
+import javax.persistence.EntityNotFoundException;
 import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
@@ -25,9 +29,11 @@ public class BoardService2 {
     @Autowired
     private BoardRepository boardRepository;
 
+    @Autowired
+    private CommentRepository commentRepository;
     public void write(Board board) {  //entity를 매개변수로 받음
 
-        boardRepository.save(board);        //새로운 게시물이 데이터베이스에 추가됩니다.
+        boardRepository.save(board);  //새로운 게시물이 데이터베이스에 추가됩니다.
     }
 
 
@@ -60,7 +66,6 @@ public class BoardService2 {
         return boardRepository.findByType("C", pageable); // "1"은 인증방 타입에 해당하는 것으로 가정합니다.
     }
 
-
     public List<Board> boardList(){
         List<Board> boardList = boardRepository.findAll();
         return boardList;
@@ -70,11 +75,11 @@ public class BoardService2 {
     }
 
     public Optional<Board> findById(Integer no) {
-
         return boardRepository.findById(no);
     }
 
     public void deleteBoard(Integer no) {
         boardRepository.deleteById(no);
     }
+
 }
--- src/main/java/com/kb04/starroad/Service/CommentService.java
+++ src/main/java/com/kb04/starroad/Service/CommentService.java
@@ -1,8 +1,50 @@
 package com.kb04.starroad.Service;
 
+import com.kb04.starroad.Dto.board.CommentDto;
+import com.kb04.starroad.Entity.Board;
+import com.kb04.starroad.Entity.Comment;
+import com.kb04.starroad.Repository.BoardRepository;
+import com.kb04.starroad.Repository.CommentRepository;
+import com.kb04.starroad.Repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
 
 @Service
 public class CommentService {
+
+    @Autowired
+    private CommentRepository commentRepository;
+
+    @Autowired
+    private BoardRepository boardRepository;
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    public List<CommentDto> findByBoard(Board board) {
+
+        return commentRepository.findByBoard(board);
+    }
+
+
+    @Transactional
+    public int createComment(String content, int boardNo) {
+
+        CommentDto newComment = new CommentDto();
+
+        newComment.setContent(content);
+        newComment.setRegdate(new java.util.Date());
+//      newComment.setMember(memberRepository.findByNo(1));
+        newComment.setBoard(boardRepository.findByNo(boardNo));;
+
+        Comment comment = newComment.toEntity();
+
+        commentRepository.save(comment);
+        return comment.getBoard().getNo();
+    }
 
 }
